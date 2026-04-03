eb2afa302bf400a6780ebf37d162f96d6b9aeefb
rigelb126 <rigelb126@gmail.com>
2026-04-03 16:32:56 -0700

    Component Login listo

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
+import { OAuthService, AuthConfig } from 'angular-oauth2-oidc';
+import { AuthService } from '../../services/auth';
+import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
+
+const googleAuthConfig: AuthConfig = {
+  issuer: 'https://accounts.google.com',
+  redirectUri: window.location.origin,
+  clientId: environment.googleClientId,
+  scope: 'openid profile email',
+  strictDiscoveryDocumentValidation: false
+};
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.css',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
 })
-export class Login {}
+export class LoginComponent {
+
+  constructor(
+    private oauthService: OAuthService,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.oauthService.configure(googleAuthConfig);
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  loginConGoogle() {
+    this.oauthService.initImplicitFlow();
+  }
+}
