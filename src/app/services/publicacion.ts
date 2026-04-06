1cd26ba15e8097aa741fdd46019bc01ac4b04ecd
rigelb126 <rigelb126@gmail.com>
2026-04-05 18:43:55 -0700

    Metodos para likes y comentarios en Service Publicacion

--- src/app/services/publicacion.ts
+++ src/app/services/publicacion.ts
@@ -17,7 +17,33 @@ export class PublicacionService {
     let params = new HttpParams();
     if (carrera) params = params.set('carrera', carrera);
     if (importancia) params = params.set('importancia', importancia);
-
     return this.http.get<Publicacion[]>(`${this.apiUrl}/publicaciones/feed`, { params });
+  }
+
+  darLike(idPublicacion: number): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/publicaciones/${idPublicacion}/like`, {});
+  }
+
+  quitarLike(idPublicacion: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/publicaciones/${idPublicacion}/like`);
+  }
+
+  getComentarios(idPublicacion: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/publicaciones/${idPublicacion}/comentarios`);
+  }
+
+  comentar(idPublicacion: number, texto: string, idPadre?: number): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/publicaciones/${idPublicacion}/comentarios`, {
+      texto,
+      idPadre: idPadre ?? null
+    });
+  }
+
+  darLikeComentario(idComentario: number): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/publicaciones/comentarios/${idComentario}/like`, {});
+  }
+
+  quitarLikeComentario(idComentario: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/publicaciones/comentarios/${idComentario}/like`);
   }
 }
