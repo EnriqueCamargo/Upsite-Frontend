9d57f27e262ffa38c27da75192ec2160c2fd105f
rigelb126 <rigelb126@gmail.com>
2026-04-05 17:14:22 -0700

    Interfaz Usuario lista

--- src/app/interfaces/usuario.ts
+++ src/app/interfaces/usuario.ts
@@ -1,1 +1,11 @@
-export interface Usuario {}
+export interface Usuario {
+  id: number;
+  nombres: string;
+  apellidos: string;
+  email: string;
+  fotoPerfil: string;
+  rol: string;
+  matricula: string;
+  grupo: string;
+  carrera: string;
+}
