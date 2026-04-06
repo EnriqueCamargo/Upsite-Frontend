4ea4a97e93afd3bab6cf893a8188d230263e4bb5
rigelb126 <rigelb126@gmail.com>
2026-04-05 17:16:50 -0700

    Service Publicacion listo

--- src/app/services/publicacion.ts
+++ src/app/services/publicacion.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Publicacion } from '../interfaces/publicacion';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Publicacion {}
+export class PublicacionService {
+
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getFeed(carrera?: number, importancia?: string): Observable<Publicacion[]> {
+    let params = new HttpParams();
+    if (carrera) params = params.set('carrera', carrera);
+    if (importancia) params = params.set('importancia', importancia);
+
+    return this.http.get<Publicacion[]>(`${this.apiUrl}/publicaciones/feed`, { params });
+  }
+}
