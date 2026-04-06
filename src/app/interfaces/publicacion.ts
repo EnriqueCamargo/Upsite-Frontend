05d9e4c7f4d36bc3eee17bbf5486647efb9a0cc7
rigelb126 <rigelb126@gmail.com>
2026-04-05 17:11:36 -0700

    Interfaz publicacion lista

--- src/app/interfaces/publicacion.ts
+++ src/app/interfaces/publicacion.ts
@@ -1,1 +1,11 @@
-export interface Publicacion {}
+export interface MultimediaPublicacion {
+  id: number;
+  ruta: string;
+}
+
+export interface Publicacion {
+  id: number;
+  texto: string;
+  importancia: string;
+  multimedia: MultimediaPublicacion[];
+}
--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment: {
-  production: boolean;
-  apiUrl: string;
-  googleClientId: string;
-} = {
-  production: false,
-  apiUrl: 'http://localhost:8080',
-  googleClientId: '1003180221427-f787imeh68otcmshd95jeioqjsn6k7b4.apps.googleusercontent.com'
-};
