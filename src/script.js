ca0733ceb352586d4a1349a6c1fcb1adc1ad2f83
Jorge Villalpando <jorginmoises@gmail.com>
2026-04-10 01:13:01 -0700

    Se termino la pantalla de denuncias

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function toggleAccordion(id) {
+    const content = document.getElementById(id);
+    
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
